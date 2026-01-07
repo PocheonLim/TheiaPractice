7106ceda4d005e5870be16c07ca543f6aa2a7fe1
임성훈 <liam01@tobesoft.com>
2026-01-07 18:47:23 +0900

    feature: nexa-database

---
+++ packages/nexa-database/src/browser/nexa-database-contribution.ts
@@ -0,0 +1,43 @@
+// *****************************************************************************
+// Copyright (C) 2018 Ericsson and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0.
+//
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License v. 2.0 are satisfied: GNU General Public License, version 2
+// with the GNU Classpath Exception which is available at
+// https://www.gnu.org/software/classpath/license.html.
+//
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
+// *****************************************************************************
+
+import { injectable } from '@theia/core/shared/inversify';
+import { AbstractViewContribution } from '@theia/core/lib/browser';
+import { NexaDatabaseWidget } from './nexa-database-widget';
+import { Command, CommandRegistry } from '@theia/core';
+
+export const NexaDatabaseCommand: Command = { id: 'widget:command ' };
+
+@injectable()
+export class NexaDatabaseContribution extends AbstractViewContribution<NexaDatabaseWidget> {
+
+    constructor() {
+        super({
+            widgetId: NexaDatabaseWidget.ID,
+            widgetName: NexaDatabaseWidget.LABEL,
+            defaultWidgetOptions: {
+                area: 'main'
+            },
+            toggleCommandId: NexaDatabaseCommand.id
+        });
+    }
+
+    override registerCommands(commands: CommandRegistry): void {
+        commands.registerCommand(NexaDatabaseCommand, {
+            execute: () => super.openView({ activate: false, reveal: true })
+        });
+    }
+}
